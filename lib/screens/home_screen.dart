25121508d4efd488a0b39836c3e0939bae8f5faf
Neidn <dty3152@gmail.com>
2023-06-13 16:27:07 +0900

    pagination

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '/utils/colors.dart';
+
 import '/providers/user_provider.dart';
+
+import '/screens/feed_screen.dart';
+import '/screens/go_live_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   static const routeName = '/home';
@@ -13,15 +18,72 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late final PageController _pageController;
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const FeedScreen(),
+    const GoLiveScreen(),
+    const Text('Browse'),
+  ];
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: _currentIndex);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void onPageChanged(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  void jumpToPage(int index) {
+    _pageController.jumpToPage(index);
+  }
+
   @override
   Widget build(BuildContext context) {
     final UserProvider userProvider = Provider.of<UserProvider>(context);
 
     return Scaffold(
-      body: Center(
-        child: Text(
-          userProvider.user.username,
+      body: SafeArea(
+        child: PageView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: _pageController,
+          onPageChanged: onPageChanged,
+          children: _pages,
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        selectedItemColor: buttonColor,
+        unselectedItemColor: primaryColor,
+        backgroundColor: backgroundColor,
+        currentIndex: _currentIndex,
+        onTap: jumpToPage,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Following',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_rounded),
+            label: 'Go Live',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.copy),
+            label: 'Browse',
+          ),
+        ],
       ),
     );
   }
